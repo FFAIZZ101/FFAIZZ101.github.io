313fb3947b0cb45d4d74af98dc2f051e35549b1b
FFAIZZ <10dit23f2023@student.psp.edu.my>
2025-04-10 09:46:22 +0800

    10/4/2025

---
+++ Function.js
@@ -0,0 +1,37 @@
+function addition () {
+    let A = 5;
+    let B = 3;
+
+    console.log(A + B);
+}
+
+function subtraction (A , B) {
+console.log(A - B);
+
+}
+
+function multiplication () {
+    let A = 2;
+    let B = 5;
+    let result = A * B;
+
+    //increment operator
+    //result++
+
+    //decrement operator
+    result--
+    console.log (result)
+}
+
+function division (A , B) {
+    console.log (A / B);
+}
+
+function countdown () {
+    let countdown = 10;
+
+ while(countdown > 0) {
+    console.log ("countdown " + countdown);
+    countdown--;
+ }
+}
